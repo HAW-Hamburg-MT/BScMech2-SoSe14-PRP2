8cab8cb018457f70a128a07963003dfa40d73bf3
jtByt.Pictures <jtByt.Pictures@gmail.com>
2014-04-13 10:33:57 +0200

    netbeans changes and new PRP2-A2

---
+++ PRP2-A2/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: jannik
+ *
+ * Created on 13. April 2014, 10:29
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
