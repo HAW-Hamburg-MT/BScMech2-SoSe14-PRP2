853bcb6c1973d2b069fa7fa2ffe07a5dbbf28592
jtByt.Pictures <jtByt.Pictures@gmail.com>
2014-04-16 21:45:27 +0200

    housekeeping

--- PRP2-A2/stateMachine.h
+++ PRP2-A2/stateMachine.h
@@ -16,7 +16,7 @@
 #ifndef BScMech2_SoSe14_PRP2_stateMachine_h
 #define BScMech2_SoSe14_PRP2_stateMachine_h
 
-//all states: 0,     1,      2,      height,   h. OK,     h. nOK,    junction, metal no,  metal yes
+//all states: 0,     1,      2,      height,   h. OK,     h. n. OK,  junction, metal no,  metal yes
 typedef enum {START, ACTIVE, TRANSP, TRANSP_H, TRANSP_H1, TRANSP_H0, TRANSP_J, TRANSP_M0, TRANSP_M1, AT_SLIDE, AT_END} States;
 
 
